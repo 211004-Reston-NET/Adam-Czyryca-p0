e8c56383dddee1bbc31e931419dcde1bd686fd87
AdamCzyryca <62111741+aczy623@users.noreply.github.com>
2021-10-06 17:02:12 -0400

    base structure

---
+++ RRModel/Resturaunt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RRModel
+{
+    public class Resturaunt 
+    {
+        private string _city;
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+        
+    }
+}
